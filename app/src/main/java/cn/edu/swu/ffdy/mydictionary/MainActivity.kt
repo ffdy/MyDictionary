89c15c72c39f5e7ba0aeb60ac5ed422c53b605ae
ffdy <1836017424@qq.com>
2022-07-07 02:04:04 +0800

    环境重构

--- app/src/main/java/cn/edu/swu/ffdy/mydictionary/MainActivity.kt
+++ app/src/main/java/cn/edu/swu/ffdy/mydictionary/MainActivity.kt
@@ -2,10 +2,32 @@ package cn.edu.swu.ffdy.mydictionary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+    }
+
+    private fun initView() {
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when(item.itemId) {
+                R.id.main_dict -> {
+                    // Respond to navigation item 1 click
+                    true
+                }
+                R.id.main_search -> {
+                    // Respond to navigation item 2 click
+                    true
+                }
+                R.id.main_setting -> {
+                    true
+                }
+                else -> false
+            }
+        }
     }
 }
